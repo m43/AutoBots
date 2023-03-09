458d115e943845d962ea06ae789d48e275c4baad
m43 <frano.rajich@gmail.com>
2023-03-09 18:59:33 +0100

    Support synth-v1 dataset and dataloader .

--- datasets/synth/dataset.py
+++ datasets/synth/dataset.py
@@ -1,33 +1,35 @@
 import os
+
 import numpy as np
-import glob
 from torch.utils.data import Dataset
 
 
-class TrajNetPPDataset(Dataset):
-    def __init__(self, dset_path, split_name="train"):
-        self.num_others = 5
+class SynthV1Dataset(Dataset):
+    def __init__(self, dset_path, filename):
+        # TODO: Note that the number of agents is hardcoded to match the
+        #       standard Synth-v1. This parameter as well as the dataset
+        #       preprocessing parameter `--max-number-of-agents` must be
+        #       tweaked if a different dataset is used, e.g., one of the
+        #       out-of-distribution datasets related to Synth-v1. 
+        self.num_others = 11
+
         self.pred_horizon = 12
         self.num_agent_types = 1  # code assuming only one type of agent (pedestrians).
-        self.in_seq_len = 9
+        self.in_seq_len = 8
         self.predict_yaw = False
         self.map_attr = 0  # dummy
         self.k_attr = 2
+        dataset_path = os.path.join(dset_path, filename)
+        print(f"SynthV1Dataset: Loading dataset from {os.path.abspath(dataset_path)}")
+        self.agents_dataset = np.load(dataset_path)[:, :, :self.num_others + 1]
 
-        dset_fnames = sorted(glob.glob(os.path.join(dset_path, split_name+"_*.npy")))
-        agents_dataset = []
-        for dset_fname in dset_fnames:
-            agents_dataset.append(np.load(dset_fname))
-        self.agents_dataset = np.concatenate(agents_dataset)[:, :, :self.num_others+1]
-        del agents_dataset
-
-    def __getitem__(self, idx: int):
-        data = self.agents_dataset[idx]
+    def unpack_datapoint(self, trajectories):
+        assert len(trajectories) == self.in_seq_len + self.pred_horizon
 
         # Remove nan values and add mask column to state
-        data_mask = np.ones((data.shape[0], data.shape[1], 3))
-        data_mask[:, :, :2] = data
-        nan_indices = np.where(np.isnan(data[:, :, 0]))
+        data_mask = np.ones((trajectories.shape[0], trajectories.shape[1], 3))
+        data_mask[:, :, :2] = trajectories
+        nan_indices = np.where(np.isnan(trajectories[:, :, 0]))
         data_mask[nan_indices] = [0, 0, 0]
 
         # Separate past and future.
@@ -42,6 +44,9 @@ class TrajNetPPDataset(Dataset):
 
         return ego_in, ego_out, agents_in[:, 1:], agents_out[:, 1:], roads, agent_types
 
+    def __getitem__(self, idx: int):
+        trajectories = self.agents_dataset[idx]
+        return self.unpack_datapoint(trajectories)
+
     def __len__(self):
         return len(self.agents_dataset)
-
--- process_args.py
+++ process_args.py
@@ -14,7 +14,7 @@ def get_train_args():
 
     # Section: Dataset
     parser.add_argument("--dataset", type=str, required=True, choices=["Argoverse", "Nuscenes", "trajnet++",
-                                                                       "interaction-dataset"],
+                                                                       "interaction-dataset", "synth"],
                         help="Dataset to train on.")
     parser.add_argument("--dataset-path", type=str, required=True, help="Path to dataset files.")
     parser.add_argument("--use-map-image", type=bool, default=False, help="Use map image if applicable.")
@@ -47,7 +47,8 @@ def get_train_args():
     parser.add_argument("--learning-rate-sched", type=int, nargs='+', default=[5, 10, 15, 20],
                         help="Learning rate Schedule.")
     parser.add_argument("--grad-clip-norm", type=float, default=5, metavar="C", help="Gradient clipping norm")
-    parser.add_argument("--num-epochs", type=int, default=150, metavar="I", help="number of iterations through the dataset.")
+    parser.add_argument("--num-epochs", type=int, default=150, metavar="I",
+                        help="number of iterations through the dataset.")
     args = parser.parse_args()
 
     if args.use_map_image and args.use_map_lanes:
@@ -62,6 +63,8 @@ def get_train_args():
     elif "interaction-dataset" in args.dataset:
         assert "Ego" not in args.model_type, "Can't run AutoBot-Ego on Interaction Dataset..."
         assert not args.use_map_image, "Interaction-dataset does not have image-based scene information..."
+    elif args.dataset == "synth":
+        assert not args.use_map_image and not args.use_map_lanes, "Synth-v1 has no scene map information..."
 
     results_dirname = create_results_folder(args)
     save_config(args, results_dirname)
